921df7c24925fa7f7c43ade422d80d467a230f4c
FAHMI Aimen <aimen.fahmi@unifr.ch>
2025-01-12 20:46:17 +0100

    Document manager barebones

---
+++ src/app/document-manager/layout.js
@@ -0,0 +1,22 @@
+import Sidebar from "@/components/Sidebar";
+import Link from "next/link";
+import { BiArrowBack } from "react-icons/bi";
+
+function Layout({ children }) {
+  return (
+    <div className="grid grid-cols-[auto_1fr]">
+      <Sidebar>
+        <Link
+          className="ml-20 mt-4 flex items-center justify-center gap-4 text-nowrap hover:text-text_light"
+          href={"/ai-assistant"}
+        >
+          <BiArrowBack className="h-6 w-6" />
+          <span>Virtual Assistant</span>
+        </Link>
+      </Sidebar>
+      <main>{children}</main>
+    </div>
+  );
+}
+
+export default Layout;
---
+++ src/components/DocumentList.js
@@ -0,0 +1,5 @@
+function DocumentList() {
+  return <div></div>;
+}
+
+export default DocumentList;
