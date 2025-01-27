8721d81736c43c83de06e66217621513a03944d8
FAHMI Aimen <aimen.fahmi@unifr.ch>
2025-01-27 08:51:58 +0100

    fix: better login page design

--- src/app/(routes)/auth/_components/MarketingCarousel.js
+++ src/app/(routes)/auth/_components/MarketingCarousel.js
@@ -4,14 +4,14 @@ function MarketingCarousel() {
   return (
     <div className="relative h-full w-full">
       <Image
-        src={"/computer-02.jpg"}
+        src={"/app-02.png"}
         fill
         className="object-cover"
         alt="background image"
       />
 
-      <div className="absolute left-1/2 top-36 -translate-x-1/2 -translate-y-1/2 transform bg-white">
-        <div className="flex flex-col items-center justify-center gap-6">
+      <div className="absolute left-1/2 top-20 -translate-x-1/2 -translate-y-1/2 transform bg-transparent">
+        <div className="flex flex-col items-center justify-center gap-4">
           <h1 className="text-nowrap text-3xl font-[500] text-text">
             Document Management
           </h1>
@@ -19,10 +19,6 @@ function MarketingCarousel() {
             Upload documents to give the TCS assistant awarenes of your business
           </span>
         </div>
-      </div>
-
-      <div className="absolute left-1/2 top-1/2 h-[39%] w-[69%] -translate-x-[48.5%] -translate-y-[53%] transform bg-white">
-        <Image src={"/app-01.png"} fill alt="background image" />
       </div>
     </div>
   );
--- src/app/(routes)/auth/login/layout.js
+++ src/app/(routes)/auth/login/layout.js
@@ -4,8 +4,8 @@ import MarketingCarousel from "../_components/MarketingCarousel";
 function layout({ children }) {
   return (
     <div className="flex items-center justify-center">
-      <div className="w-[45%]">{children}</div>
-      <div className="relative -z-50 h-screen w-[55%]">
+      <div className="w-[40%]">{children}</div>
+      <div className="relative -z-50 h-screen w-[60%]">
         <MarketingCarousel />
       </div>
     </div>
